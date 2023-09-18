555ebfca4460eb239a4875f7bedf6d65c7eaf9e2
chanee <hanssang6301@gmail.com>
2023-09-18 22:34:48 +0900

    init

---
+++ src/main/java/com/be/inssagram/InssagramApplication.java
@@ -0,0 +1,13 @@
+package com.be.inssagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InssagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InssagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/be/inssagram/InssagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.inssagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InssagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
