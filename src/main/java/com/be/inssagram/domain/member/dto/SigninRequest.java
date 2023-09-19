7f8fdb11fea798e10e618b0c372c7822459b7303
tkddbs1008 <finibin08@gmail.com>
2023-09-19 14:42:30 +0900

    SigninRequest 에 builder 추가

--- src/main/java/com/be/inssagram/domain/member/dto/SigninRequest.java
+++ src/main/java/com/be/inssagram/domain/member/dto/SigninRequest.java
@@ -1,15 +1,13 @@
 package com.be.inssagram.domain.member.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class SigninRequest {
     private String email;
     private String password;
