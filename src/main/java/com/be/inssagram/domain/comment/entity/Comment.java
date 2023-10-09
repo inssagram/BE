8a9395186501c38841a96b2905ed97c20cfd0510
chanee <hanssang6301@gmail.com>
2023-10-10 08:52:16 +0900

    fix : 댓글, 대댓글 CRUD 작성, 수정, 삭세, 포스트내 부모 댓글 리스트 조회, 한 댓글에서 대댓글 리스트 조회.

---
+++ src/main/java/com/be/inssagram/domain/comment/entity/Comment.java
@@ -0,0 +1,57 @@
+package com.be.inssagram.domain.comment.entity;
+
+import com.be.inssagram.common.BaseEntity;
+import com.be.inssagram.domain.comment.dto.request.CommentRequest;
+import com.be.inssagram.domain.member.entity.Member;
+import com.be.inssagram.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@SQLDelete(sql = "UPDATE inssagram.comment SET deleted_at = current_timestamp WHERE comment_id = ?")
+@Where(clause = "deleted_at is NULL")
+@Entity(name = "COMMENT")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 댓글은 하나의 Post에 속합니다.
+//    @JsonBackReference
+    private Post post;
+//    private Long postId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+//    private Long memberId;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 대댓글의 부모 댓글
+    @JoinColumn
+    private Comment parentComment; // 부모 댓글
+
+    // 대댓글을 작성할 수 있는 필드
+    @OneToMany(mappedBy = "parentComment", orphanRemoval = true)
+    private List<Comment> childComments = new ArrayList<>();
+
+    // 댓글과 대댓글을 구분하기 위한 필드
+    private boolean replyFlag;
+
+    public void updateFields(CommentRequest request) {
+        if (request.getContents() != null) {
+            content = request.getContents();
+        }
+    }
+
+}
--- src/main/java/com/be/inssagram/domain/post/entity/Post.java
+++ src/main/java/com/be/inssagram/domain/post/entity/Post.java
@@ -1,12 +1,15 @@
 package com.be.inssagram.domain.post.entity;
 
 import com.be.inssagram.common.BaseEntity;
+import com.be.inssagram.domain.comment.entity.Comment;
 import com.be.inssagram.domain.post.dto.request.UpdatePostRequest;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -33,8 +36,10 @@ public class Post extends BaseEntity {
     @ElementCollection
     private List<String> likedByPerson;
     private Integer likeCount;
-    @ElementCollection
-    private List<String> comments;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post",
+            cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JsonManagedReference
+    private List<Comment> comments = new ArrayList<>();
     @ElementCollection
     private Set<String> taggedMembers;
     @ElementCollection
