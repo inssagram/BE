4a90c4fb2633d821e6b349f38a54d8c35d5569a6
tkddbs1008 <finibin08@gmail.com>
2023-11-07 22:21:43 +0900

    add/fix 팔로우 연관 관계

--- src/main/java/com/be/inssagram/domain/follow/controller/FollowController.java
+++ src/main/java/com/be/inssagram/domain/follow/controller/FollowController.java
@@ -6,6 +6,7 @@ import com.be.inssagram.config.Jwt.TokenProvider;
 import com.be.inssagram.domain.follow.dto.request.FollowRequest;
 import com.be.inssagram.domain.follow.service.FollowService;
 import com.be.inssagram.domain.member.dto.response.InfoResponse;
+import com.be.inssagram.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
 import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,7 @@ public class FollowController {
     @PostMapping("/member/follow")
     public ApiResponse<?> followMember(@RequestHeader("Authorization") String token,
                                        @RequestBody FollowRequest request){
-        InfoResponse member = InfoResponse.fromEntity(tokenProvider.getMemberFromToken(token));
+        Member member = tokenProvider.getMemberFromToken(token);
         String result = followService.follow(member, request);
         return ApiResponse.createMessage(result);
     }
--- src/main/java/com/be/inssagram/domain/follow/service/FollowService.java
+++ src/main/java/com/be/inssagram/domain/follow/service/FollowService.java
@@ -3,10 +3,12 @@ package com.be.inssagram.domain.follow.service;
 import com.be.inssagram.domain.follow.dto.request.FollowRequest;
 import com.be.inssagram.domain.follow.entity.Follow;
 import com.be.inssagram.domain.follow.repository.FollowRepository;
-import com.be.inssagram.domain.member.dto.response.InfoResponse;
+import com.be.inssagram.domain.hashTag.entity.HashTag;
+import com.be.inssagram.domain.hashTag.repository.HashTagRepository;
 import com.be.inssagram.domain.member.entity.Member;
 import com.be.inssagram.domain.member.repository.MemberRepository;
 import com.be.inssagram.domain.notification.service.NotificationService;
+import com.be.inssagram.exception.common.DataDoesNotExistException;
 import com.be.inssagram.exception.member.UserDoesNotExistException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,13 @@ public class FollowService {
     private final FollowRepository followRepository;
     private final MemberRepository memberRepository;
     private final NotificationService notificationService;
+    private final HashTagRepository hashTagRepository;
 
     //팔로잉 및 해제
-    public String follow(InfoResponse myInfo, FollowRequest request){
+    public String follow(Member myInfo, FollowRequest request){
         Member memberInfo = memberRepository.findById(request.getFollowId()).orElseThrow(
                 UserDoesNotExistException::new);
-        Follow exists = followRepository.findByMyIdAndMemberId(myInfo.member_id(), memberInfo.getId());
+        Follow exists = followRepository.findByRequesterInfoAndFollowingInfo(myInfo, memberInfo);
         if(exists != null){
             followRepository.delete(exists);
             return "팔로잉을 해지하셧습니다";
@@ -33,36 +36,31 @@ public class FollowService {
                 followRepository.save(follow);
                 notificationService.notify(notificationService
                         .createNotifyDto(
-                                request.getFollowId(),
-                                "follow",
-                                follow.getId(),
+                                memberInfo,
                                 null,
-                                myInfo.member_id(),
-                                myInfo.nickname(),
-                                myInfo.image(),
-                                myInfo.nickname()+"님이 회원님을 팔로우 하셧습니다"
+                                myInfo,
+                                myInfo.getNickname()+"님이 회원님을 팔로우 하셧습니다"
                         ));
                 return "팔로잉 하셧습니다";
             }
-            followRepository.save(setFollowHashtag(myInfo.member_id(), request.getHashtagId()));
+            HashTag hashTag = hashTagRepository.findById(request.getHashtagId())
+                    .orElseThrow(DataDoesNotExistException::new);
+            followRepository.save(setFollowHashtag(myInfo, hashTag));
             return "팔로잉 하셧습니다";
         }
     }
 
 
-    private Follow setFollowMember(InfoResponse myInfo, Member memberInfo){
+    private Follow setFollowMember(Member myInfo, Member memberInfo){
         return Follow.builder()
-                .myId(myInfo.member_id())
-                .myName(myInfo.nickname())
-                .memberId(memberInfo.getId())
-                .memberImage(memberInfo.getImage())
-                .memberName(memberInfo.getNickname())
+                .requesterInfo(myInfo)
+                .followingInfo(memberInfo)
                 .build();
     }
 
-    private Follow setFollowHashtag(Long myId, Long hashtagId){
+    private Follow setFollowHashtag(Member myInfo, HashTag hashtagId){
         return Follow.builder()
-                .myId(myId)
+                .requesterInfo(myInfo)
                 .hashtagId(hashtagId)
                 .build();
     }
