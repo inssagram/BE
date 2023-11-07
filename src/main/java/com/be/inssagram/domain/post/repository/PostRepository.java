7b9e48c30ce71b99d847375f6c8a76ef36dda667
hanchanhee <hanssang6301@gamil.com>
2023-11-08 05:02:20 +0900

    refactor: post 기능에 type 추가
    - PostType enum 추가: post, reels 구분.

--- src/main/java/com/be/inssagram/domain/post/repository/PostRepository.java
+++ src/main/java/com/be/inssagram/domain/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.be.inssagram.domain.post.repository;
 
 import com.be.inssagram.domain.post.entity.Post;
+import com.be.inssagram.domain.post.type.PostType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByMemberId(Long memberId);
+    List<Post> findByMemberIdAndType(Long memberId, PostType type);
 }
