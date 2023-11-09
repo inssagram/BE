4bbc6024f972040a5f11f93a0c568cfe36043347
tkddbs1008 <finibin08@gmail.com>
2023-11-10 02:20:23 +0900

    change JWT filter for websocket

--- src/main/java/com/be/inssagram/config/Jwt/filter/JwtAuthenticateFilter.java
+++ src/main/java/com/be/inssagram/config/Jwt/filter/JwtAuthenticateFilter.java
@@ -62,7 +62,7 @@ public class JwtAuthenticateFilter extends OncePerRequestFilter {
     private boolean isPublicPath(String requestURI) {
         // Define the public URL patterns that do not require a token
         List<String> publicPaths = Arrays.asList("/signup", "/signin", "/search", "/member/detail", "/"
-                , "/post/search-detail", "/post/all");
+                , "/post/search-detail", "/post/all", "/ws-stomp", "/exchange", "/pub");
 
         // Check if the request URI matches a public path
         return publicPaths.stream().anyMatch(requestURI::startsWith);
