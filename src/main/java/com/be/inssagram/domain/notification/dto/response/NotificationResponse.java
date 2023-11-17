ddbce48c258f7f6896420d1d38b41ccb5d311809
tkddbs1008 <finibin08@gmail.com>
2023-11-17 18:13:17 +0900

    fix 알림 메세지 null 값 제외한 응답값 반환

--- src/main/java/com/be/inssagram/domain/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/be/inssagram/domain/notification/dto/response/NotificationResponse.java
@@ -3,12 +3,17 @@ package com.be.inssagram.domain.notification.dto.response;
 import com.be.inssagram.domain.member.dto.response.InfoResponse;
 import com.be.inssagram.domain.member.entity.Member;
 import com.be.inssagram.domain.notification.entity.Notification;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+
+import java.util.Optional;
 
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record NotificationResponse(
         Long id,
+        Long list_id,
         String created_at,
         boolean friend_status,
         boolean read_status,
@@ -25,18 +30,7 @@ public record NotificationResponse(
                 ? followNotification(notification)
                 : (notification.getChatroomId() != null)
                 ? chatroomList(notification)
-                : NotificationResponse.builder()
-                .id(notification.getId())
-                .created_at(notification.getCreatedAt())
-                .friend_status(notification.isFriendStatus())
-                .read_status(notification.isReadStatus())
-                .post_id(notification.getPostInfo().getId())
-                .post_image(notification.getPostInfo().getImage().get(0))
-                .message(notification.getMessage())
-                .sender_id(notification.getSenderInfo().getId())
-                .sender_image(notification.getSenderInfo().getImage())
-                .sender_name(notification.getSenderInfo().getNickname())
-                .build();
+                : buildWithNonNullValues(notification);
     }
 
     private static NotificationResponse followNotification(Notification notification) {
@@ -56,7 +50,7 @@ public record NotificationResponse(
 
     private static NotificationResponse chatroomList(Notification notification) {
         return NotificationResponse.builder()
-                .id(notification.getId())
+                .list_id(notification.getId())
                 .created_at(notification.getCreatedAt())
                 .friend_status(notification.isFriendStatus())
                 .read_status(notification.isReadStatus())
@@ -66,6 +60,25 @@ public record NotificationResponse(
                 .sender_image(notification.getSenderInfo().getImage())
                 .sender_name(notification.getSenderInfo().getNickname())
                 .build();
+    }
+
+    private static NotificationResponse buildWithNonNullValues(Notification notification) {
+        NotificationResponseBuilder builder = NotificationResponse.builder()
+                .id(notification.getId())
+                .created_at(notification.getCreatedAt())
+                .friend_status(notification.isFriendStatus())
+                .read_status(notification.isReadStatus())
+                .message(notification.getMessage())
+                .sender_id(notification.getSenderInfo().getId())
+                .sender_image(notification.getSenderInfo().getImage())
+                .sender_name(notification.getSenderInfo().getNickname());
+
+        if (notification.getPostInfo() != null) {
+            builder.post_id(notification.getPostInfo().getId());
+            builder.post_image(notification.getPostInfo().getImage().get(0));
+        }
+
+        return builder.build();
     }
 }
 
