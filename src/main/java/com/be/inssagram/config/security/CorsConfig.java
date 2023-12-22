54853ec99d40021a07ca7f1c0f722646ed88e71c
tkddbs1008 <87680989+tkddbs1008@users.noreply.github.com>
2023-12-22 20:37:23 +0900

    Update CorsConfig.java

--- src/main/java/com/be/inssagram/config/security/CorsConfig.java
+++ src/main/java/com/be/inssagram/config/security/CorsConfig.java
@@ -19,6 +19,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("http://localhost:3000",
                 "https://inssagram-rb9u8ngxq-joshyeom.vercel.app/",
                 "https://inssagram-fe.vercel.app/",
+                "inssagram-two.vercel.app/",
                 "https://fe-lyart-nu.vercel.app/",
                 "https://fe-joshyeom.vercel.app/"));
         config.addAllowedHeader("*");
